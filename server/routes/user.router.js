e69ca4993f89b51c3291fbb58e4ce57c64b06cfa
Kyle Hagler <khagler.kh@gmail.com>
2020-07-30 16:09:55 -0500

    Refactor login and register for email
    Co-authored-by: Christopher Neisen <christopherjay71186@gmail.com>
    Co-authored-by: Dalton J Meyer <dalton.meyer94@gmail.com>
    Co-authored-by: Clever Aaron Mukori <clever.mukori@ndus.edu>

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -16,11 +16,11 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 // The only thing different from this and every other post we've seen
 // is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {  
-  const username = req.body.username;
+  const email = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
 
-  const queryText = 'INSERT INTO "user" (username, password) VALUES ($1, $2) RETURNING id';
-  pool.query(queryText, [username, password])
+  const queryText = 'INSERT INTO "user" (email, password) VALUES ($1, $2) RETURNING id';
+  pool.query(queryText, [email, password])
     .then(() => res.sendStatus(201))
     .catch(() => res.sendStatus(500));
 });
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -49,7 +49,7 @@ class LoginPage extends Component {
           <h1>Login</h1>
           <div>
             <label htmlFor="username">
-              Username:
+              Email:
               <input
                 type="text"
                 name="username"
