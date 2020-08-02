1bc4435187ff052941b1574d435097338ca3bd69
Kyle Hagler <khagler.kh@gmail.com>
2020-08-01 22:06:57 -0500

    Thought table needed the ability to reorder I guess this is part of the built in function of MUI table

--- src/components/AdminHome/AdminHome.js
+++ src/components/AdminHome/AdminHome.js
@@ -31,7 +31,8 @@ componentWillMount () {
                     
          
       
-
+{/*PLEASE NOTE: instead of start date, we want tos how latest activity on this table */}
+{/*This will be tied to whenever a student logs in, it will do a put on that column to show thier latest login */}
      <MUITable
             data={this.props.students
               .filter(
@@ -54,9 +55,9 @@ componentWillMount () {
                  entry.grad_year,
                  entry.school_attend,
                  entry.lcf_id,
-                 moment(entry.lcf_start_date).format("MMMM Do YYYY"),
+                 moment(entry.lcf_start_date).format("MMMM Do YYYY"), //This will change "last login" at some point
                  entry.student_email,
-                 entry.password,
+                 //entry.password,
                  entry.pif_amount
               ])}
             columns={[
@@ -65,8 +66,8 @@ componentWillMount () {
               "Grade",
               "Graduation Year",
               "School Name",
-              "LCF ID:",
-              "LCF Start Date",
+              "LCF ID",
+              "LCF Start Date", //This will change "last login" at some point
               "Student Email",
               "Student PIF Amount ($)"
             ]}
