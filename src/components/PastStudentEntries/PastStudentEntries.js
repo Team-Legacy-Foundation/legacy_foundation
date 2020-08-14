ec0f9fb855dca3c7c423e0707329666b8a8f1d3e
Kyle Hagler <khagler.kh@gmail.com>
2020-08-14 09:52:19 -0500

    Take out local from moment. Will check on Heroku

--- src/components/PastStudentEntries/PastStudentEntries.js
+++ src/components/PastStudentEntries/PastStudentEntries.js
@@ -88,7 +88,7 @@ class PastStudentEntries extends Component {
           <TableBody>
            {(entry.length !== 0)&& <TableRow style={{backgroundColor: 'yellow'}} >
             <TableCell style={gunnarStyle}>{moment(entry.pay_day).format("MMMM Do YYYY")}</TableCell>
-            <TableCell style={gunnarStyle}>{moment(entry.date_submitted).local().format("MMMM Do YYYY")}</TableCell>
+            <TableCell style={gunnarStyle}>{moment(entry.date_submitted).format("MMMM Do YYYY")}</TableCell>
             <TableCell style={gunnarStyle}>{entry.pass_class}</TableCell>
             <TableCell style={gunnarStyle}>{entry.gpa}</TableCell>
             <TableCell style={gunnarStyle}>{entry.clean_attend}</TableCell>
