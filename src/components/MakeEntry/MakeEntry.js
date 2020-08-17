3b1eee0476b17b7c21d1a7dad7d6b80a4d039f76
Kyle Hagler <khagler.kh@gmail.com>
2020-08-17 14:05:53 -0500

    sorry was on master

--- src/components/MakeEntry/MakeEntry.js
+++ src/components/MakeEntry/MakeEntry.js
@@ -162,7 +162,7 @@ class MakeEntry extends Component {
     }
 
 
-    let historyEntries = this.props.studentHistory;
+    let historyEntries = this.props.studentHistory; 
     let date = moment();
     let previous_pay_day = moment("2020-08-10T00:00:00.000")
     let pay_day = moment(previous_pay_day)
